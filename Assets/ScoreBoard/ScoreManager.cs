1f27ddbf98f4001e697679fa3b62affabfd2d6df
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2014-12-18 21:51:15 +0100

    ScoreBoard quill18 https://www.youtube.com/watch?v=gjFsrVWQaQw

---
+++ Assets/ScoreBoard/ScoreManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScoreManager : MonoBehaviour {
+
+	Dictionary < string, Dictionary < string , int > > playerScore;
+
+	void Init()
+	{
+		if(playerScore != null)
+			return;
+		playerScore = new Dictionary< string , Dictionary <string , int > >();
+	}
+
+	public int GetScore( string username, string scoreType){
+		Init ();
+
+		if (playerScore.ContainsKey (username) == false) {
+			//We have no score recordet at all for this username
+			return 0;
+		}
+		if (playerScore [username].ContainsKey (scoreType) == false) {
+			return 0;		
+		}
+
+		return playerScore [username] [scoreType];
+	}
+
+	public void SetScore( string username, string scoreType, int value){
+		Init ();
+
+		if (playerScore.ContainsKey (username) == false) {
+			playerScore[username] = new Dictionary<string, int>();
+		}
+		playerScore [username] [scoreType] = value;
+	}
+
+	public void ChangeScore( string username, string scoreType, int amount){
+		Init ();
+
+		int currScore = GetScore (username, scoreType);
+		SetScore (username, scoreType, currScore + amount);
+	}
+
+}
