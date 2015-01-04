acfd3b8d42a3a12a3d02a391418196f93b24c3b9
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2015-01-04 06:08:28 +0100

    pathfinding

---
+++ Assets/Tiled Pathfinding/TileMap.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMap : MonoBehaviour {
+
+	public TileType[] tileType;
+
+	int[,] tiles;
+	
+	int mapSizeX = 10;
+	int mapSizeY = 10;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//Allocate our Map tiles
+		tiles = new int[mapSizeX, mapSizeX];
+
+		// Initalize our map Tiles
+		for (int x=0; x < mapSizeX; x++) {
+			for( int y=0; y < mapSizeY; y++ ){
+				tiles[x,y] = 0;
+			}
+		}
+
+		// Ushaped Mountain
+
+		tiles [4, 4] = 2;
+		tiles [5, 4] = 2;
+		tiles [6, 4] = 2;
+		tiles [7, 4] = 2;
+		tiles [8, 4] = 2;
+
+		tiles [8, 5] = 2;
+		tiles [8, 6] = 2;
+
+		tiles [4, 5] = 2;
+		tiles [4, 6] = 2;
+
+		GenareteMapVisual ();
+	}
+	void GenareteMapVisual(){
+		for (int x=0; x < mapSizeX; x++) {
+			for( int y=0; y < mapSizeY; y++ ){
+				Instantiate( tileType[ tiles[x, y] ].tileVisulPrefab, new Vector3(x,y,0), Quaternion.identity);
+			}
+		}
+	}
+}
---
+++ Assets/Tiled Pathfinding/TileType.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable] //Segir unity að það se i lagi að syna þetta script i edidor
+public class TileType 
+{
+	public string name;
+	public GameObject tileVisulPrefab;
+
+	//public bool isWalkable = true;
+}
